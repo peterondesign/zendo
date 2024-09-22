ed50dc9b894be06a8bca845028f604f44c30dd87
Your Name <you@example.com>
2024-09-22 22:17:25 +0100

    Next UI Clean Install

---
+++ .next/static/chunks/app_page_tsx_815e6c._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_815e6c._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_815e6c._.js",
+  "chunks": [
+    "static/chunks/node_modules_cb4b5c._.js",
+    "static/chunks/app_page_tsx_9fdd4f._.js"
+  ],
+  "source": "dynamic"
+});

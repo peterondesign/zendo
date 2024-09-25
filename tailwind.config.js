8ecfa4c8215e1ed17ce5c2a3cb347ec1d2bbeb56
Your Name <you@example.com>
2024-09-25 09:31:53 +0100

    Vercel cleanup 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ import {nextui} from '@nextui-org/theme'
 module.exports = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
